ab9a13259f8dd434b8e9a68e2d12806e40195d73
lucascastro29 <88244464+lucascastro29@users.noreply.github.com>
2024-01-08 15:58:55 -0300

    second push

---
+++ app/index/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page(){
+
+    return(
+    <div>Hello world
+
+
+    </div>
+    )
+}
---
+++ app/ui/layout/NavBar.tsx
@@ -0,0 +1,9 @@
+export default function Page(){
+
+    return(
+    <div className="d-flex justify-content-center align-items-center">
+
+
+    </div>
+    )
+}
