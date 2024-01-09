a55d54b46f57e724d79be18bd8c32754daf3a531
emzi16 <emilianozapata0638@gmail.com>
2024-01-09 16:14:05 -0300

    Burger button action modified & syntax errors nav & footer

--- app/ui/layout/NavBar.tsx
+++ app/ui/layout/NavBar.tsx
@@ -1,97 +1,109 @@
-'use client'
+"use client";
 import Link from "next/link";
-import icon from "@/public/Icon.jpg"
+import icon from "@/public/Icon.jpg";
 import Image from "next/image";
 
-//--->> Esto de aca se encarga de hacer que funcione el boton hamburguesa, selecciona el 
+//--->> Esto de aca se encarga de hacer que funcione el boton hamburguesa, selecciona el
 //elemento que es el disparador y el objeto
-import { Collapse } from 'flowbite';
-import type { CollapseOptions, CollapseInterface } from 'flowbite';
-import type { InstanceOptions } from 'flowbite';
-import { useEffect } from "react";
-
-const targetEl:HTMLElement = document.getElementById('navbar-cta');
-
-// optionally set a trigger element (eg. a button, hamburger icon)
-const triggerEl: HTMLElement = document.getElementById('button_trigger');
-
-// optional options with default values and callback functions
-const options: CollapseOptions = {
-    onCollapse: () => {
-        console.log('element has been collapsed');
-    },
-    onExpand: () => {
-        console.log('element has been expanded');
-    },
-    onToggle: () => {
-        console.log('element has been toggled');
-    },
-};
-
-// instance options object
-const instanceOptions: InstanceOptions = {
-  id: 'targetEl',
-  override: true
-};
-
-/*
- * $targetEl: required
- * $triggerEl: optional
- * options: optional
- * instanceOptions: optional
- */
-const collapse: CollapseInterface = new Collapse(
-    targetEl,
-    triggerEl,
-    options,
-    instanceOptions
-);
-
+// import { Collapse } from "flowbite";
+// import type { CollapseOptions, CollapseInterface } from "flowbite";
+// import type { InstanceOptions } from "flowbite";
+import { useEffect, useState } from "react";
 
 //--->
 //para mi hay que ponerle una animacion al header para cuando inicie a scrollear cambie de color o algo, porque asi es muy estatico
 
-export default function Page(){
+export default function Page() {
+  //cambio de estado para que se abra el boton hamburguesa
+  const [showBurgerButtton, setShowBurgerButton] = useState(false);
 
-    return(
-        <nav className="bg-white border-gray-200 dark:bg-gray-900 fixed w-full" style={{backgroundColor:"#001220"}}> 
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4" >
-          <Link href={"#Home"} id="Home">
-                <Image src={icon} width={110} height={110} alt="icon_image"/>
+  return (
+    <nav
+      className="bg-white border-gray-200 dark:bg-gray-900 fixed w-full"
+      style={{ backgroundColor: "#001220" }}
+    >
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link href={"#Home"} id="Home">
+          <Image src={icon} width={110} height={110} alt="icon_image" />
+        </Link>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Get started
+          </button>
+          <button
+            data-collapse-toggle="navbar-cta"
+            id="button_trigger"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-cta"
+            onClick={() => setShowBurgerButton(!showBurgerButtton)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+            showBurgerButtton ? "block" : "hidden"
+          }`}
+          id="navbar-cta"
+        >
+          <ul
+            className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+            style={{ backgroundColor: "#001220" }}
+          >
+            <li>
+              <Link
+                href={"#Home"}
+                className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Home
               </Link>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-              <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
-              <button data-collapse-toggle="navbar-cta"  id="button_trigger" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" >
-                <span className="sr-only">Open main menu</span>
-                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
-            </button>
-          </div>
-          <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta" >
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"style={{backgroundColor:"#001220"}}>
-              <li>
-              
-              <Link href={"#Home"} className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</Link>
-              </li>
-              <li>
-              <Link href={"#About"} className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
-
-
-              </li>
-              <li>
-                <Link href={"#Services"} className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</Link>
-
-              </li>
-              <li>
-              <Link href={"#Contact"} className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</Link>
-
-
-              </li>
-            </ul>
-          </div>
-          </div>
-        </nav>
-
-    )
+            </li>
+            <li>
+              <Link
+                href={"#About"}
+                className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"#Services"}
+                className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                href={"#Contact"}
+                className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
