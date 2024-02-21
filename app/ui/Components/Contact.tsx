f7434c58ef252f4935d06d9f96593888ca312e9e
lucascastro29 <88244464+lucascastro29@users.noreply.github.com>
2024-02-20 21:31:43 -0300

    us

--- app/ui/Components/Contact.tsx
+++ app/ui/Components/Contact.tsx
@@ -3,20 +3,8 @@ export default function Page() {
   //En mi opinion hay que agregar una imagen de fondo a la seccion de contacto
   //Y mejorar el h2 y otras cosas
 
-  return (<div className="flex justify-left w-full">
-    <div
-      className="p-10 m-14 border rounded"
-      id="Contact"
-      style={{
-        boxShadow:"0px 4px 20px rgba(0, 0, 0, 0.25)"
-      }}
-    >
-      <h2 className="grid place-items-center text-3xl md:text-4xl lg:text-5xl font-bold mt-8 mb-8">
-        {" "}
-        Make your project real!
-      </h2>
-      <Form />
-    </div>
+  return (<div className="flex justify-left w-full" >
+   
     <div className="flex justify-center w-full">
       <div
       className="p-10 m-14 border rounded"
--- app/ui/Components/Us.tsx
+++ app/ui/Components/Us.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     <RevealWrapper delay={600} origin="bottom"   reset>
 
     <section
-    className="w-full flex flex-row py-48 md:py-48 lg:py-48  "
+    className="w-full flex flex-row-reverse content-end align-center justify-right py-48 md:py-48 lg:py-48  "
     style={{
       height:"1080px",
       background:
@@ -17,25 +17,13 @@ export default function Page() {
     backgroundSize: "100%",
     }}
   > 
-  <div className="px-0 md:px-20"></div>
-    <div className="px-4 md:px-6 flex flex-col align-center">
-      <div className="flex flex-col items-center  justify-top space-y-4 h-12 md:h-96 md:justify-center text-left " >
+    <div className="px-4 md:px-6 mt-24 mr-20 flex flex-col align-center">
+      <div className="flex flex-col items-center  justify-top space-y-4 h-12 md:h-96 md:justify-center text-right " >
         <div className="space-y-4 flex-col" >
           <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-white ">
-            Launch your Brand
+            take your Internet brand on
           </h1>
-          <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-            We create stunning websites and applications that help you stand out in the digital
-            world.
-          </p>
-          <div className="mt-20">
-          <Link
-            className="text-white bg-gradient-to-r mt-20 from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4  focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
-            href="#Contact"
-          >
-            Get Started
-          </Link>
-          </div>
+       
         </div>
          
           
